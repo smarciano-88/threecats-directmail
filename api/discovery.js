8a410706ee9633407923d1f23e3a13595a79cb25
smarciano-88 <64026094+smarciano-88@users.noreply.github.com>
2026-04-07 19:56:08 -0400

    Update discovery.js

--- api/discovery.js
+++ api/discovery.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'OPTIONS') {
